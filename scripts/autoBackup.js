547403122f4e22205eaeba493b6b69a1bc460af0
zhuyuezy <2595223212@qq.com>
2020-09-28 15:39:33 +0800

    Form auto backup script's commit

--- scripts/autoBackup.js
+++ scripts/autoBackup.js
@@ -12,8 +12,7 @@ function run() {
 		exit(1);
 	} else {
 		echo("======================Auto Backup Begin===========================");
-		exec('D:')
-		cd('hexoBlog');    //此处修改为Hexo根目录路径
+		cd('D:\hexoBlog'); //此处修改为Hexo根目录路径
 		if (exec('git add --all').code !== 0) {
 			echo('Error: Git add failed');
 			exit(1);
